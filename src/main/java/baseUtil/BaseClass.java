9dcc9ba15b483e39d57f88fdfb85c54271ca936b
ruhul@Rafas_laptop <mdrhlkdus@gmail.comgit config --global user.email mdrhlkdus@gmail.comgit config --global user.email mdrhlkdus@gmail.com>
2023-11-24 03:50:50 -0500

    home work03

--- src/main/java/baseUtil/BaseClass.java
+++ src/main/java/baseUtil/BaseClass.java
@@ -1,22 +1,30 @@
 package baseUtil;
 
 import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
 import pages.HomePage;
 
 public class BaseClass {
-	public WebDriver driver;
-	public HomePage homePage;
+	public ChromeDriver driver; // or we can use protected type
+	public HomePage homePage; // or we can use protected type
 
 	@BeforeMethod
 	public void setUP() {
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\ruhul\\eclipse-workspace\\com.libertymutual\\driver\\chromedriver.exe");
+
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver.exe");
+
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		// We can also use fullscreen() instead of maximize()
+		// driver.manage().window().fullscreen();
 		driver.manage().deleteAllCookies();
 		driver.get("https://www.libertymutual.com/");
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
@@ -28,4 +36,5 @@ public class BaseClass {
 	public void tearUp() {
 		driver.quit();
 	}
+
 }
